0085102dba925d89c84896a35a0efc9f772033f5
peoush-04 <peoushchakraborty2903@gmail.com>
2024-10-27 01:35:32 +0530

    useEffect done

--- useffect_prac/src/App.js
+++ useffect_prac/src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    
-  );
+  const [count, setCount] = useState(0);
+
+  //cleaning up
+  useEffect(() => {
+    console.log("added");
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
+  console.log("added2");
+
+  return () =>{
+    console.log("removed"); 
+    clearTimeout(timer);
+  }
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
 }
+
 
 export default App;
